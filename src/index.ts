abbc8f4b7fefe2f022010d196c76503c097e6271
Sam Zagrobelny <strangesast@gmail.com>
2019-12-09 10:38:33 -0500

    example working

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import * as d3 from 'd3';
 
 const body = d3.select(document.body);
 
-body.append('p').text('D3 starter works');
+body.append('p').text('Scrolls');
 
 const height = 500, width = 500;
 
@@ -22,14 +22,30 @@ ctx.lineWidth = lineWidth;
 ctx.moveTo(0, 0);
 ctx.lineTo(0, height/2 - radius - lineWidth);
 ctx.translate(0, height/2 - radius - lineWidth);
+ctx.stroke();
 
-function spiral(radius) {
-  const step = -Math.PI / 2;
-  let x = radius, y = 0;
-  for (let angle = Math.PI; angle > -Math.PI; angle += step) {
-    ctx.arc(radius, 0, radius, angle, angle + step, true);
-    console.log(`angle ${angle}`);
+function spiral(radius, step=Math.PI/4) {
+  const direction = -1;
+  const steps = Math.PI*2/step;
+
+  step = direction * step;
+
+
+  // translate to first center
+  ctx.save();
+  ctx.translate(radius, 0);
+
+  let r, nextRadius = radius;
+  let angle = Math.PI;
+
+  for (let i = 0; i < steps; i++) {
+    r = nextRadius
+    ctx.arc(0, 0, r, angle, angle+step, direction == -1);
+    nextRadius = r * 0.95;
+    angle = angle + step;
+    ctx.translate(Math.cos(angle)*(r - nextRadius), Math.sin(angle)*(r - nextRadius));
   }
+  ctx.restore();
 }
 
 spiral(radius);
